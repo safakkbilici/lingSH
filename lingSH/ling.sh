666f1ca2a28769f72292723ff2c88abe0adde6ed
Safak Bilici <safakk.bilici@gmail.com>
2020-07-10 21:42:09 +0300

    u2l added

--- lingSH/ling.sh
+++ lingSH/ling.sh
@@ -2,6 +2,7 @@ echo "--------------------------------------------------------------------------
 echo "Welcome to the text data analysis API, lingSH."
 echo "lingSH provides a large text analysis tools."
 echo "LingSH creates and writes a file your specific data analysis choices in lingSH.txt"
+echo "If you change a file, (i.e, punctuation marks to '\n' translation), the software will use changed file"
 echo "-----------------------------------------------------------------------------------"
 
 if [ "$#" -ne 1 ]
@@ -15,7 +16,7 @@ if [[ $1 != *.txt ]]
 then
     echo "Type-Exist Error: You must pass a ~*.txt file as an argument. This file is not a .txt file"
 fi
-
+i=0
 while true; do
     echo "For frequency listing, type : 'f'"
     echo "For tokenizing, press : 't'"
@@ -24,15 +25,26 @@ while true; do
     read -r input
     if [[ $input == "f" ]]
     then
-	echo "For listing specific words'/word's frequency listing, type : 'r'"
+	echo "For listing specific words'/word's frequency listing, type 'r' :"
 	read -r regex
 	if [[ $regex == 'r' ]]
 	then
 	    echo "Type its RegEx format (i.e, '\<[Aa]nd\>'): "
 	    read -r regex
-	    tr -sc 'A-Za-z' '\n' < shakes.txt | sort | uniq --count | sort -n -r | grep $regex > lingSH.txt
+	    tr -sc 'A-Za-z' '\n' < $1 | sort | uniq --count | sort -n -r | grep $regex > lingSH.txt
+	else
+	    tr -sc 'A-Za-z' '\n' < $1 | sort | uniq --count | sort -n -r > lingSH.txt
+	fi	
+    fi
+    if [[ $input == 'c' ]]
+    then
+	echo "For upper-case to lower-case, type 'u2l' : "
+	read -r ul
+	if [[ $ul == "u2l" ]]
+	then
+	    tr "[:upper:]" "[:lower:]" < $1 > "$1$i"txt
+	    i=i+1
 	fi
-	
     fi
     
 done
